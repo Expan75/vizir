ece3f4b7eb4ac309770cca17096f9750da01d183
hakan <erik.hakansson96@gmail.com>
2022-02-10 20:00:52 +0100

    minor cleaning

--- video-encoder/main.py
+++ video-encoder/main.py
@@ -36,7 +36,7 @@ def get_uploaded_image(bucketname: str, filename: str) -> Image:
         log.error("no file found of name %s in bucket %s, exiting..." % (filename, bucketname))
         exit()
 
-def encode_image(loaded_image: Image):
+def generate_image_embeddings(loaded_image: Image):
     """Encodes an image """
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model, preprocess = clip.load("ViT-B/32", device=device)
@@ -45,46 +45,50 @@ def encode_image(loaded_image: Image):
         image_features = model.encode_image(collapsed_image).tolist()[0]
     return image_features
 
+def prepare_feature_export(encoded_image: list) -> list:
+    """Returns BQ exportable list of image encodings as a list of rows"""
+    features = {}
+    features['embeddings'] = [{ "value": value } for value in encoded_image]
+    features['frame_uuid'] = str(uuid())
+    features['video_uuid'] = str(uuid())
+    features['frame_timestamp'] = datetime.now().isoformat()
+    
+    return [features]
+
 def on_upload(event: dict, context):
     """Responds to movie clip uploads to GCP buckets and encodes the contents.
        The encoded content is exported to BigQuery were it enables semantic search on frames.
     """
     bucketname = event.get('bucket')
     filename = event.get('name')
+
+    # download and process image into bq exportable rows
     image = get_uploaded_image(bucketname, filename)
+    embeddings = generate_image_embeddings(image)
+    rows = prepare_feature_export(embeddings)
 
-    # encode using model
-    encoded_image = encode_image(image)
-    
-    # setup export data structure
-    features = {}
-    features['frame_embeddings'] = encoded_image
-    features['frame_uuid'] = str(uuid())
-    features['video_uuid'] = str(uuid())
-    features['video_section_start'] = datetime.now().isoformat()
-    features['video_section_stop'] = datetime.now().isoformat()
-    
-    pass
-
-if __name__ == '__main__':
-    image = Image.open('./clip.png')
-
-    # encode using model
-    encoded_image = encode_image(image)
-
-    # setup export data structure
-    features = {}
-    features['embeddings'] = [{ "value": value } for value in encoded_image]
-    features['frame_uuid'] = str(uuid())
-    features['video_uuid'] = str(uuid())
-    features['frame_timestamp'] = datetime.now().isoformat()
-
-    rows = [
-        features
-    ]
-
+    # write to BigQuery
     bq = bigquery.Client.from_service_account_json(GCP_CREDENTIALS)
     errors = bq.insert_rows_json(BQ_TABLE_ID, rows)
     if errors:
         print(errors)
         raise Exception('Errors occured when writing to BQ')
+    else:
+        print(f'bq write successful.')
+    pass
+
+if __name__ == '__main__':
+    image = Image.open('./clip.png')
+
+    # encode using model and process features into exportables
+    encoded_image = generate_image_embeddings(image)
+    rows = prepare_feature_export(encoded_image)
+
+    # wrte to BigQuery
+    bq = bigquery.Client.from_service_account_json(GCP_CREDENTIALS)
+    errors = bq.insert_rows_json(BQ_TABLE_ID, rows)
+    if errors:
+        print(errors)
+        raise Exception('Errors occured when writing to BQ')
+    else:
+        print(f'bq write successful.')
