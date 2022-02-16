096f839dc9db9596c124d6ea4a593c6fde49c4cb
hakan <erik.hakansson96@gmail.com>
2022-02-16 11:48:33 +0100

    add basic styling and page layout, including form handling

--- query-encoder/main.py
+++ query-encoder/main.py
@@ -1,7 +1,22 @@
-from flask import render_template
+from flask import render_template, abort
 import functions_framework
-
 
 @functions_framework.http
 def index(request):
-    return render_template("landing.html", title="Vizir Home")
+    if request.method == "POST":
+        query = request.form.get("query") # no validation; spooky stuff
+        recommendations = [
+            { "title": "item1" },
+            { "title": "item2" },
+            { "title": "item3" },
+        ]
+        return render_template(
+            "results.html", 
+            title="Result | Vizir",
+            recommendations=recommendations,
+            query=query
+        )
+    elif request.method == "GET":
+        return render_template("landing.html", title="Search | Vizir")
+    else:
+        abort(404)
