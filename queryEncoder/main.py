422062eaed4c767c05ce30079a21d35e09cff032
hakan <erik.hakansson96@gmail.com>
2022-02-17 11:02:34 +0100

    fixed imports via folder renaming

--- queryEncoder/main.py
+++ queryEncoder/main.py
@@ -1,11 +1,23 @@
-from flask import render_template, abort
+import os
+import logging
+import search
 import functions_framework
+from google.cloud import bigquery
+from flask import render_template, abort
+
+log = logging.getLogger(__name__)
+
+# Settings
+GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
+GCP_CREDENTIALS = os.getenv("GCP_CREDENTIALS", "./creds/secret.json")
+BQ_DATASET_ID = os.getenv("BQ_DATASET_ID", "vizir_development")
+BQ_TABLE_ID = f"{GCP_PROJECT_ID}.{BQ_DATASET_ID}.video_embeddings"
 
 @functions_framework.http
 def index(request):
     if request.method == "POST":
         query = request.form.get("query") # no validation; spooky stuff
-        recommendations = [{ "title": f"item{n}" } for n in range(0,50)]
+        recommendations = search.by_string(query)
         return render_template(
             "results.html", 
             title="Result | Vizir",
---
+++ queryEncoder/search.py
@@ -0,0 +1,4 @@
+
+
+def by_string(query: str) -> list:
+  return [{ "title": f"item{n}" } for n in range(0,50)]
